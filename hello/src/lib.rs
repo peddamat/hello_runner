35261d1d9f9881d4221dd6a78af45fef996f0f6a
Sam Peddamatham <peddamat@gmail.com>
2023-02-24 15:04:27 -0800

    Works when calling SetWinHook from hello_inject.

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,4 +1,4 @@
-use windows::Win32::System::LibraryLoader::{GetProcAddress, LoadLibraryA};
+use windows::Win32::System::LibraryLoader::{GetProcAddress, LoadLibraryA, DisableThreadLibraryCalls};
 use windows::{ Win32::Foundation::*, Win32::System::SystemServices::*, };
 use windows::{ core::*, Win32::UI::WindowsAndMessaging::MessageBoxA, };
 use windows::{ Win32::Graphics::Gdi::*, Win32::UI::WindowsAndMessaging::*, };
@@ -28,74 +28,25 @@ extern "system" fn DllMain(
     _: *mut ())
     -> bool
 {
-    match call_reason {
-        DLL_PROCESS_ATTACH => attach(dll_module),
-        DLL_PROCESS_DETACH => info!("Detaching!"),
-        _ => ()
+    unsafe {
+        match call_reason {
+            DLL_PROCESS_ATTACH => attach(dll_module),
+            DLL_PROCESS_DETACH => info!("Process detach called: {}", GetCurrentProcessId()),
+            DLL_THREAD_ATTACH  => info!("Thread attach called: {}", GetCurrentThreadId()),
+            DLL_THREAD_DETACH  => info!("Thread detach called: {}", GetCurrentThreadId()),
+            _ => ()
+        }
     }
 
     true
 }
 
 fn attach(dll_module: HINSTANCE) {
-    eventlog::init("Example Log", log::Level::Trace).unwrap();
-    info!("Attach called!");
+    eventlog::init("Example Log", log::Level::Info).unwrap();
     unsafe {
-        let tid = GetCurrentThreadId();
-        let pid = GetCurrentProcessId();
-        info!("Thread id: {}", tid);
-        info!("Process id: {}", pid);
-
-        let hwnd = find_window_by_pid(pid).unwrap();
-        info!("Found window handle");
-
-        // Get thread id
-        let mut pid = MaybeUninit::<u32>::zeroed();
-        let gui_tid = GetWindowThreadProcessId(hwnd, Some(pid.as_mut_ptr()));
-        info!("GUI Thread id: {}", gui_tid);
-
-        // let stub_module = unsafe { LoadLibraryA(PCSTR("hello.dll\0".as_ptr() as *const u8)).unwrap() };
-        // let stub_callback: HOOKPROC = unsafe { std::mem::transmute(GetProcAddress(dll_module, PCSTR("callback".as_ptr()))) };
-        // HOOK = match SetWindowsHookExW(WH_CALLWNDPROC, stub_callback, None, gui_tid) {
-
-        // Doesn't work when injected
-        // Works when Load Library is called
-        let shitter = GetProcAddress(dll_module, s!("callback")).unwrap();
-        // let dick: unsafe extern "system" fn(i32, WPARAM, LPARAM) -> LRESULT = transmute(&shitter);
-        let dick: HOOKPROC = transmute(shitter);
-        HOOK = match SetWindowsHookExW(WH_GETMESSAGE, dick, HINSTANCE(0), gui_tid) {
-        // HOOK = match SetWindowsHookExW(WH_GETMESSAGE, dick, dll_module, gui_tid) {
-
-        // Doesn't work when injected
-        // Works when LoadLibrary is used
-        // HOOK = match SetWindowsHookExW(WH_CALLWNDPROC, Some(callback), dll_module, gui_tid) {
-
-        // Doesn't work when injected
-        // Works when LoadLibrary is used
-        // HOOK = match SetWindowsHookExW(WH_CALLWNDPROC, Some(callback), dll_module, GetCurrentThreadId()) {
-
-        // HOOK = match SetWindowsHookExW(WH_CALLWNDPROC, HOOKPROC::Some(callback), 0, gui_tid) {
-            Ok(handle) => handle,
-            Err(_) => {
-                info!("Error in SetWindowsHookEx");
-                return
-            }
-        };
-
-        info!("Farty");
-        PostMessageA(hwnd, WM_SIZING,WPARAM(0),LPARAM(0));
-        // MessageBoxA(HWND(0), s!("Woo!"), s!("hello_dll"), Default::default());
-
-        // Get handle to main window
-        // let hwnd = find_window_by_pid(pid).unwrap();
-
-        // if SetWindowSubclass(hwnd, Some(subclass_proc), 0, 0,).as_bool() {
-        //     MessageBoxA(HWND(0), s!("Woo!"), s!("hello_dll"), Default::default());
-        // }
-        // else {
-        //     MessageBoxA(HWND(0), s!("Fail!"), s!("hello_dll"), Default::default());
-        // }
-    };
+        info!("hello.dll loaded by process: {}", GetCurrentProcessId());
+        DisableThreadLibraryCalls(dll_module);
+    }
 }
 
 #[no_mangle]
@@ -116,55 +67,6 @@ unsafe extern "system" fn callback(n_code: i32, w_param: WPARAM, l_param: LPARAM
     CallNextHookEx(HHOOK::default(), n_code, w_param, l_param)
 }
 
-// https://github.com/alexrsagen/binarymagic-tas/blob/517a0dce912192aadee75e0a147abf71d931c016/src/sys.rs
-pub fn find_window_by_pid(pid: u32) -> Result<HWND> {
-    let mut data = MaybeUninit::new(EnumWindowsData {
-        wanted_pid: pid,
-        handle: HWND::default(),
-        found: false,
-    });
-    let data = unsafe {
-        EnumWindows(
-            Some(enum_windows_callback),
-            LPARAM(data.as_mut_ptr() as isize),
-        );
-        data.assume_init()
-    };
-    if !data.found {
-        return Err(Error::new(
-            ErrorKind::NotFound,
-            "no window found matching pid",
-        ));
-    }
-    Ok(data.handle)
-}
-
-#[derive(Default)]
-struct EnumWindowsData {
-    wanted_pid: u32,
-    handle: HWND,
-    found: bool,
-}
-
-unsafe extern "system" fn enum_windows_callback(handle: HWND, lparam: LPARAM) -> BOOL {
-    let data = lparam.0 as *mut EnumWindowsData;
-    let mut data = data.as_mut().unwrap();
-
-    let mut pid = MaybeUninit::<u32>::zeroed();
-    GetWindowThreadProcessId(handle, Some(pid.as_mut_ptr()));
-    let pid = pid.assume_init();
-
-    if pid == data.wanted_pid
-        && GetWindow(handle, GW_OWNER).0 == 0
-        && IsWindowVisible(handle).as_bool()
-    {
-        data.handle = handle;
-        data.found = true;
-        return BOOL(0);
-    }
-
-    BOOL(1)
-}
 
 extern "system" fn subclass_proc(window: HWND, msg: u32, wparam: WPARAM, lparam: LPARAM, _subclass_id: usize, _ref_data: usize) -> LRESULT {
     if msg == WM_PAINT {
--- hello_inject/src/main.rs
+++ hello_inject/src/main.rs
@@ -3,33 +3,73 @@ use std::{thread, time, env};
 use windows::Win32::UI::WindowsAndMessaging::PostMessageA;
 use windows::Win32::Foundation::*;
 
+use windows::Win32::System::LibraryLoader::{GetProcAddress, LoadLibraryA};
+use windows::Win32::UI::WindowsAndMessaging::{FindWindowA, GetWindowThreadProcessId, PostThreadMessageA, SetWindowsHookExA, HOOKPROC, WH_CALLWNDPROC, WH_GETMESSAGE, WM_NULL};
+use windows::Win32::Foundation::{WPARAM, LPARAM};
+use windows::core::PCSTR;
+use windows::core::s;
+
 fn main() {
+    // eventlog::register("Example Log").unwrap();
     // find target process by name
-    let target_process =
+    // let target_process =
         // OwnedProcess::find_first_by_name("notepad.exe").unwrap();
-        OwnedProcess::find_first_by_name("sample_create_window.exe").unwrap();
+        // OwnedProcess::find_first_by_name("sample_create_window.exe").unwrap();
 
     // create a new syringe for the target process
-    let syringe = Syringe::for_process(target_process);
+    // let syringe = Syringe::for_process(target_process);
 
     println!("Current directory: {}", std::env::current_dir().unwrap().display());
 
     // inject the payload into the target process
     // let injected_payload = syringe.inject("hello_inject\\target\\debug\\deps\\hello.dll").unwrap();
     // let injected_payload = syringe.inject("target\\debug\\deps\\hello.dll").unwrap();
-    let injected_payload = syringe.inject("..\\target\\debug\\deps\\hello.dll").unwrap();
-    println!("DLL injected successfully!");
+    // let injected_payload = syringe.inject("..\\target\\debug\\deps\\hello.dll").unwrap();
+    // println!("DLL injected successfully!");
 
-    unsafe { PostMessageA(HWND(0xffff), 0, WPARAM(0), LPARAM(0)) };
-    unsafe { PostMessageA(HWND(0xffff), 0, WPARAM(0), LPARAM(0)) };
-    unsafe { PostMessageA(HWND(0xffff), 0, WPARAM(0), LPARAM(0)) };
 
+    let dll_location = "..\\target\\debug\\deps\\hello.dll";
+    let dll_path_str = std::ffi::CString::new(dll_location).unwrap();
+    let hw = unsafe{ FindWindowA(PCSTR(std::ptr::null()), PCSTR("Untitled - Notepad\0".as_ptr())) };
+    let mut process_id = 0;
+    let tid = unsafe { GetWindowThreadProcessId(hw, Some(&mut process_id)) };
+
+    println!("Found window: {}", tid);
+
+    if tid == 0 {
+        panic!("{}","Failed to get thread id");
+    }
+
+
+    unsafe {
+        let stub_module = match LoadLibraryA(PCSTR(dll_path_str.as_ptr() as *const u8)) {
+            Ok(handle) => handle,
+            Err(_) => {
+                println!("Error in LoadLibraryA: {:?}", GetLastError());
+                return
+            }
+        };
+        let stub_callback: HOOKPROC = std::mem::transmute(GetProcAddress(stub_module, PCSTR("callback\0".as_ptr()))) ;
+
+        let shitter = GetProcAddress(stub_module, s!("callback")).unwrap();
+        let dick: unsafe extern "system" fn(i32, WPARAM, LPARAM) -> LRESULT = std::mem::transmute(&shitter);
+        // let dick: HOOKPROC = std::mem::transmute(shitter);
+
+        let result = match SetWindowsHookExA(WH_CALLWNDPROC, stub_callback, stub_module, tid) {
+            Ok(handle) => handle,
+            Err(_) => {
+                println!("Error in SetWindowsHookEx: {:?}", GetLastError());
+                return
+            }
+
+        };
+    }
 
     // do something else
-    let ten_millis = time::Duration::from_secs(60);
+    let ten_millis = time::Duration::from_secs(30);
     println!("Sleeping for 30 secs...");
     thread::sleep(ten_millis);
 
     // eject the payload from the target (optional)
-    syringe.eject(injected_payload).unwrap();
+    // syringe.eject(injected_payload).unwrap();
 }
